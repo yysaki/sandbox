97162b95364ce7970f758da061b423f22c17a018
yysaki <mail@yysaki.com>
2022-05-04 12:12:23 +0900

    rfa: clients/vision.rbを仕立て上げる

---
+++ ruby/rfa/clients/vision.rb
@@ -0,0 +1,51 @@
+# frozen_string_literal: true
+
+require 'csv'
+require 'google/cloud/vision'
+
+module Client
+  class Vision
+    class UnexpectedError < ::StandardError; end
+
+    class Activity
+      ATTRIBUTES = %i[status_id created_at activity_time consumption_calory].freeze
+      attr_reader(*ATTRIBUTES)
+
+      def initialize(status_id:, created_at:, activity_time:, consumption_calory:)
+        @status_id = status_id
+        @created_at = created_at
+        @activity_time = activity_time
+        @consumption_calory = consumption_calory
+      end
+
+      def to_csv
+        CSV.generate do |csv|
+          csv << ATTRIBUTES.map(&:to_s)
+          csv << ATTRIBUTES.map { |attr| send attr }
+        end
+      end
+
+      def self.build(tweet:, descriptions:)
+        description = descriptions.select { |desc| desc.include? '合計活動時間' }.first
+        return if description.nil?
+
+        consumption_calory = description.scan(/(\d+\.\d+)kcal/).first.first
+        activity_time = description.scan(/((\d+)時間)?(\d+)分(\d+)秒/).first.drop(1).compact.join(':')
+
+        Activity.new(status_id: tweet.status_id, created_at: tweet.created_at, activity_time:, consumption_calory:)
+      end
+    end
+
+    def show(tweet)
+      image_annotator = Google::Cloud::Vision.image_annotator
+      descriptions = tweet.photo_urls.map do |url|
+        response = image_annotator.text_detection(image: url)
+        response.responses.first.text_annotations.first.description
+      end
+
+      Activity.build(tweet:, descriptions:)
+    rescue StandardError => e
+      raise UnexpectedError, e.message
+    end
+  end
+end
--- ruby/rfa/main.rb
+++ ruby/rfa/main.rb
@@ -1,71 +1,13 @@
 # frozen_string_literal: true
 
-require 'google/cloud/vision'
+require_relative './clients/twitter'
+require_relative './clients/vision'
 
-def main(images:)
-  image_annotator = Google::Cloud::Vision.image_annotator
-  descriptions = images.map do |image|
-    response = image_annotator.text_detection(image:)
-    response.responses.map do |res|
-      res.text_annotations.first.description
-    end
-  end
-
-  puts descriptions
+def main(count: 20)
+  tweets = Client::Twitter.new.list(count:)
+  vision = Client::Vision.new
+  activities = tweets.map { |tweet| vision.show(tweet) }
+  pp activities
 end
 
-main images: %w[tmp/sample1.jpeg tmp/sample2.jpeg]
-
-# 本日の運動結果
-# R 画面を撮影する
-# キミとプライム
-# 14分42秒
-# 合計活動時間
-# 42.40kcal
-# 合計消費力ロリー
-# 文
-# 0.
-# 合計走行距離
-# 50km
-# 1次へ
-# 本日の運動結果
-# キミとプライム
-# R 画面を撮影する
-# バンザイコシフリ
-# 138回(138回)
-# リングコン下押しこみ
-# 5回(49回)
-# アームツイスト
-# 138回(465回)
-# おなか押しこみスクワット
-# 10(20)
-# 英雄1のポーズ
-# 60回(324回)
-# サイレントダッシュ
-# 314m(5745m)
-# リングコン押しこみ
-# 55回(1159回)
-# * サイレントジョギング
-# 169m(3148m)
-# * スクワット
-# 30回(308回)
-# サイレントモモ上げ
-# 22m(177m)
-# リングアロー
-# 26回(558回)
-# サイレントウォーク
-# 1m(1m)
-# ワイドスクワット
-# 20回(40回)
-# リングコン上引っぱり
-# 17秒(50秒)
-# 扇のポーズ
-# 12回(48回)
-# リングコン引っぱりキープ
-# 6秒(309秒)
-# おなか押しこみ
-# 7回(144回)
-# リングコン下押しこみキープ
-# 0秒(94秒)
-# カッコ内はプレイ開始からの累計値です
-# とじる
+main(count: 20)
