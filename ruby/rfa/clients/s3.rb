c090c92cf576acd32a1b8f720bdcbd45d850e4fa
yysaki <mail@yysaki.com>
2022-05-05 16:30:47 +0900

    rfa: Client::S3::UnexpectedErrorの追加

--- ruby/rfa/clients/s3.rb
+++ ruby/rfa/clients/s3.rb
@@ -4,18 +4,24 @@ require 'fog/aws'
 
 module Client
   class S3
+    class UnexpectedError < ::StandardError; end
+
     PREFIX = 'rfa/'
 
     def create(file_name:, body:)
       directory = s3.directories.new(key: bucket_name)
       directory.files.create(key: "#{PREFIX}#{file_name}", body:)
+    rescue StandardError => e
+      raise UnexpectedError, e.message
     end
 
     def list
       directory = s3.directories.get(bucket_name, prefix: PREFIX)
       directory.files
-        .map { |file| file.key.gsub(PREFIX, '') }
-        .select { |file_name| file_name.include? '.csv' }
+               .map { |file| file.key.gsub(PREFIX, '') }
+               .select { |file_name| file_name.include? '.csv' }
+    rescue StandardError => e
+      raise UnexpectedError, e.message
     end
 
     private
