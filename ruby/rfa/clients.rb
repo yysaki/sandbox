f61a811818b440cf848586789075be9e67888465
yysaki <mail@yysaki.com>
2022-05-08 09:59:53 +0900

    yysaki/rfa-lifelogに切り出したのでruby/rfaを削除

--- ruby/rfa/clients.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-require_relative 'clients/s3'
-require_relative 'clients/twitter'
-require_relative 'clients/vision'
--- ruby/rfa/config.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-require 'settings_cabinet'
-
-class Settings < SettingsCabinet::Base
-  using SettingsCabinet::DSL
-
-  source 'config/settings.yml'
-end
